a1284a477fd35a91c4ef59b3a1927032c2fd5c1a
NachoCape <igcapezzolo@hotmail.com>
2022-09-29 22:02:32 -0300

    add README, and Day1 folder

---
+++ Day - 1/main.py
@@ -0,0 +1,6 @@
+print(f"Welcome to the Band Name Generator")
+print(f"What's is the name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet_name = input()
+print(f"Your band name could be {city} {pet_name}")
